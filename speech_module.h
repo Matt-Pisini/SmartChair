b53cdc39d688fa774367bc29401dc71559c43db8
Matt-Pisini <pisini@usc.edu>
2020-05-11 20:06:06 -0700

    Update speech_module.h

--- speech_module.h
+++ speech_module.h
@@ -1,21 +1,24 @@
-#ifndef speech_module_h
-#define speech_module_h
+#ifndef Speech_module_h
 
-void serial_init(unsigned short MYUBRR)
-void serial_out(char ch)
-void serial_out_string(char *s)
-void emic_stringout(char *str)
-Const char str1[] = "Welcome";
-Const char str2[] = "Main Menu";
-Const char str3[] = "UV Rating";
-Const char str4[] = "Tanning Session";
-Const char str5[] = "Set Timer";
-Const char str6[] = "Adjust Timer";
-Const char str7[] = "Run Timer";
-Const char str8[] = "Please Flip";
-Const char str9[] = "Tanning Done";
-Const char str10[] = "Apply Sunscreen";
-Const char str11[] = "Thank you for using smart chair";
-Const char star12[] = "Something Wrong";
 
+const char str1[] = "Welcome";
+const char str2[] = "Main Menu";
+const char str3[] = "UV Rating";
+const char str4[] = "Tanning Session";
+const char str5[] = "Set Timer";
+const char str6[] = "Adjust Timer";
+const char str7[] = "Run Timer";
+const char str8[] = "Please Flip";
+const char str9[] = "Tanning Done";
+const char str10[] = "Apply Sunscreen";
+const char str11[] = "Thank you for using smart chair";
+const char star12[] = "Something Wrong";
+
+
+void serial_init(unsigned short MYUBRR);
+void serial_out(char ch);
+void serial_out_string(char *s);
+void emic_stringout(char *str);
+
+#define Speech_module_h
 #endif
