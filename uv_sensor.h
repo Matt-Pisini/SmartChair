90428f9d76a9d8776d558b78360c89690d54e31a
jessicamow <jmow@usc.edu>
2020-05-11 14:08:26 -1000

    Update uv_sensor.h

--- uv_sensor.h
+++ uv_sensor.h
@@ -1,10 +1,3 @@
-//
-//  uv_sensor.h
-//  
-//
-//  Created by Jessica Mow on 5/10/20.
-//
-
 #ifndef uv_sensor_h
 #define uv_sensor_h
 
@@ -12,5 +5,6 @@ void i2c_init(void);
 uint8_t i2c_io(uint8_t device_addr, uint8_t *ap, uint16_t an, uint8_t *rp, uint16_t rn);
 float find_uv_value(uint8_t *uv_address);
 int get_UVI(void);
+int getTanningTime(int UVI, char *skinType);
 
 #endif /* uv_sensor_h */
